743810be176e521eb00cd01b8c27d3c5d8ac5dea
George Monday <georgebenmonday@gmail.com>
2024-05-30 13:01:38 -0500

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React today or tommorrow
+          Learn React today
         </a>
       </header>
     </div>
